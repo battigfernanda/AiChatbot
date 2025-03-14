7af4f5ef016f86d05ad1e501e3083bfd863fd49e
battigfernanda <battigfernanda3@gmail.com>
2025-03-14 12:16:19 -0400

    Initial Commit

---
+++ AiChatbot/AiChatbotApp.swift
@@ -0,0 +1,17 @@
+//
+//  AiChatbotApp.swift
+//  AiChatbot
+//
+//  Created by Fernanda Battig on 2025-03-14.
+//
+
+import SwiftUI
+
+@main
+struct AiChatbotApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ AiChatbotTests/AiChatbotTests.swift
@@ -0,0 +1,17 @@
+//
+//  AiChatbotTests.swift
+//  AiChatbotTests
+//
+//  Created by Fernanda Battig on 2025-03-14.
+//
+
+import Testing
+@testable import AiChatbot
+
+struct AiChatbotTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
